bd4db969d859e880962673ad51b2f483b86b0083
Phu Ngo <ptn123@gmail.com>
2021-09-09 20:36:37 -0500

    init

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -5,39 +5,48 @@ var crypto = require("crypto");
 // The Power of a Smile
 // by Tupac Shakur
 var poem = [
-	"The power of a gun can kill",
-	"and the power of fire can burn",
-	"the power of wind can chill",
-	"and the power of a mind can learn",
-	"the power of anger can rage",
-	"inside until it tears u apart",
-	"but the power of a smile",
-	"especially yours can heal a frozen heart",
+  "The power of a gun can kill",
+  "and the power of fire can burn",
+  "the power of wind can chill",
+  "and the power of a mind can learn",
+  "the power of anger can rage",
+  "inside until it tears u apart",
+  "but the power of a smile",
+  "especially yours can heal a frozen heart",
 ];
 
 var Blockchain = {
-	blocks: [],
+  blocks: [],
 };
 
 // Genesis block
 Blockchain.blocks.push({
-	index: 0,
-	hash: "000000",
-	data: "",
-	timestamp: Date.now(),
+  index: 0,
+  hash: "000000",
+  data: "",
+  timestamp: Date.now(),
 });
 
-// TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+function createBlock(_data) {
+  const block = {
+    index: Blockchain.blocks.length,
+    prevHash: Blockchain.blocks[Blockchain.blocks.length - 1].hash,
+    data: _data,
+    timestamp: Date.now(),
+  };
+
+  block.hash = blockHash(block);
+}
 
 // console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
-
 
 // **********************************
 
 function blockHash(bl) {
-	return crypto.createHash("sha256").update(
-		// TODO: use block data to calculate hash
-	).digest("hex");
+  return crypto
+    .createHash("sha256")
+    .update
+    // TODO: use block data to calculate hash
+    ()
+    .digest("hex");
 }
